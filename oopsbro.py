5eb8fd27c5f5ca5882555adb76c2489b448df6b4
Hemendranath Chowdary Madhineni <127891216+hemendranathgit@users.noreply.github.com>
2024-02-18 12:29:29 +0530

    hemendranath

--- oopsbro.py
+++ oopsbro.py
@@ -50,4 +50,83 @@
 #         super().__init__(length, width)
 #         self.height=height
 
-#
+#Walrus operator (:=)
+# foods=list()
+# while food:= input("what food do you like : ")!="quit":
+#      foods.append(food)
+
+# higher order function = A function that accepts a function as an argumnet or returns the function
+# def upperr(text):
+#     return text.upper()
+# def lowerr(text):
+#     return text.lower()
+# 
+# def higher_order(func):
+#     text=("Hemendranath chowdary Madhineni")
+#     print(func(text))
+# higher_order(upperr)
+#higher_order(lowerr)
+
+#lambda function = function written in 1 line using lambda keyword
+#                   accepts any m=number of arguments, but only has one expression.
+#lambda parameters : Expression
+# double = lambda x:x*2
+# print(double(5))
+# add = lambda x,y,z:x+y+z
+# print(add(2,3,4))
+
+#sorting
+# a=['a','c','b','y','f']
+# a.sort(reverse=True)
+# print(a)
+# 
+# students=[("hemendranath",'g',20),
+#           ("dhamu","d",45),
+#           ("prabhas","b",60),
+#           ("obi",'a',100)]
+# alpha = lambda alphas:alphas[2]
+# 
+# students.sort(key=alpha)
+# print(students)
+
+#map() = applies a function to each item in an iterable(list,tuple,e.t.c)
+# map(function,iterable)
+# store = [("shirts",20),("pants",25),("jackets",50)]
+# to_price= lambda data:(data[0],data[1]*20)
+# store_price=list(map(to_price,store))
+# print(store_price)
+# for i in store_price:
+#     print(i)
+#     
+
+#filter() = creates a collection of elements from an iterable for which a function return true
+#filter(function,iterable)
+# store = [("shirts",20),("pants",25),("jackets",50),("shorts",17)]
+# data = lambda cost:cost[1]>=18
+# store_map=filter(data,store)
+# for i in store_map:
+#     print(i)
+
+#list comprehension 
+# a=[1,2,3,4,5,6,7]
+# 
+# 
+# list =[x for x in a if x>4]
+# print(list)
+
+#dictionary comprehension
+# dict1 = {"hema":22,"nandineni": 26,"ramanjaneyulu": 51,"vijaya chandrika": 48}
+# 
+# for k,v in dict1.items():
+#     if v>35:
+#         print(k,v)
+        
+#zip(*iterables) = aggreate element from two or more iterables
+# name=["ragner","loki",'ivar']
+# age=(46,50,30)
+# 
+# merge = zip(name,age)
+# 
+# for i in merge:
+#     print(i)
+       
